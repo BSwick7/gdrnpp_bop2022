8bf999eeca4f9182d3810a2d899578b299296d3c
Brennan Swick <swick.70@osu.edu>
2023-03-23 18:42:06 +0000

    change file paths to work with new gdrn back end and auto format

--- core/gdrn_modeling/tools/lmo/lmo_3_vis_poses_full.py
+++ core/gdrn_modeling/tools/lmo/lmo_3_vis_poses_full.py
@@ -1,3 +1,9 @@
+from lib.egl_renderer.egl_renderer_v3 import EGLRenderer
+from transforms3d.quaternions import quat2mat
+from core.gdrn_modeling.datasets.dataset_factory import register_datasets
+from core.utils.data_utils import read_image_mmcv
+from lib.utils.mask_utils import mask2bbox_xyxy, cocosegm2mask, get_edge
+from lib.vis_utils.colormap import colormap
 import mmcv
 import os.path as osp
 import numpy as np
@@ -6,16 +12,11 @@ from tqdm import tqdm
 from detectron2.data import DatasetCatalog, MetadataCatalog
 from detectron2.structures import BoxMode
 import torch
+import pandas as pd
 
 cur_dir = osp.dirname(osp.abspath(__file__))
 sys.path.insert(0, osp.join(cur_dir, "../../../../"))
 
-from lib.vis_utils.colormap import colormap
-from lib.utils.mask_utils import cocosegm2mask, get_edge
-from core.utils.data_utils import read_image_mmcv
-from core.gdrn_modeling.datasets.dataset_factory import register_datasets
-from transforms3d.quaternions import quat2mat
-from lib.egl_renderer.egl_renderer_v3 import EGLRenderer
 
 score_thr = 0.3
 colors = colormap(rgb=False, maximum=255)
@@ -39,6 +40,17 @@ id2obj = {
 }
 objects = list(id2obj.values())
 
+
+def load_predicted_csv(fname):
+    df = pd.read_csv(fname)
+    info_list = df.to_dict("records")
+    return info_list
+
+
+def parse_Rt_in_csv(_item):
+    return np.array([float(i) for i in _item.strip(" ").split(" ")])
+
+
 width = 640
 height = 480
 tensor_kwargs = {"device": torch.device("cuda"), "dtype": torch.float32}
@@ -48,22 +60,35 @@ seg_tensor = torch.empty((height, width, 4), **tensor_kwargs).detach()
 
 model_dir = "datasets/BOP_DATASETS/lmo/models/"
 
-model_paths = [osp.join(model_dir, f"obj_{obj_id:06d}.ply") for obj_id in id2obj]
+model_paths = [
+    osp.join(model_dir, f"obj_{obj_id:06d}.ply") for obj_id in id2obj]
 
-ren = EGLRenderer(model_paths, vertex_scale=0.001, use_cache=True, width=width, height=height)
+ren = EGLRenderer(model_paths, vertex_scale=0.001,
+                  use_cache=True, width=width, height=height)
 
 # NOTE:
-pred_path = "output/gdrn/lmo/a6_cPnP_AugAAETrunc_BG0.5_lmo_real_pbr0.1_40e/inference_model_final/lmo_test/a6-cPnP-AugAAETrunc-BG0.5-lmo-real-pbr0.1-40e-test_lmo_test_preds.pkl"
+pred_path = "output/gdrn/lmo_pbr/convnext_a6_AugCosyAAEGray_BG05_mlL1_DMask_amodalClipBox_classAware_lmo/inference_model_final_wo_optim/lmo_bop_test/convnext-a6-AugCosyAAEGray-BG05-mlL1-DMask-amodalClipBox-classAware-lmo-test-iter0_lmo-test.csv"
 
 vis_dir = (
-    "output/gdrn/lmo/a6_cPnP_AugAAETrunc_BG0.5_lmo_real_pbr0.1_40e/inference_model_final/lmo_test/lmo_vis_gt_pred_full"
+    "output/gdrn/lmo_pbr/convnext_a6_AugCosyAAEGray_BG05_mlL1_DMask_amodalClipBox_classAware_lmo/inference_model_final_wo_optim/lmo_test/lmo_vis_gt_pred_full"
 )
 mmcv.mkdir_or_exist(vis_dir)
 
 print(pred_path)
-preds = mmcv.load(pred_path)
+preds_csv = load_predicted_csv(pred_path)
+preds = {}
+for item in preds_csv:
+    im_key = "{}/{}".format(item["scene_id"], item["im_id"])
+    item["time"] = float(item["time"])
+    item["score"] = float(item["score"])
+    item["R"] = parse_Rt_in_csv(item["R"]).reshape(3, 3)
+    item["t"] = parse_Rt_in_csv(item["t"]) / 1000
+    item["obj_name"] = id2obj[item["obj_id"]]
+    if im_key not in preds:
+        preds[im_key] = []
+    preds[im_key].append(item)
 
-dataset_name = "lmo_test"
+dataset_name = "lmo_bop_test"
 print(dataset_name)
 register_datasets([dataset_name])
 
@@ -75,6 +100,7 @@ dset_dicts = DatasetCatalog.get(dataset_name)
 for d in tqdm(dset_dicts):
     K = d["cam"]
     file_name = d["file_name"]
+    scene_im_id = d["scene_im_id"]
     img = read_image_mmcv(file_name, format="BGR")
 
     scene_im_id_split = d["scene_im_id"].split("/")
@@ -84,10 +110,14 @@ for d in tqdm(dset_dicts):
     imH, imW = img.shape[:2]
     annos = d["annotations"]
     masks = [cocosegm2mask(anno["segmentation"], imH, imW) for anno in annos]
+    fg_mask = sum(masks).astype("bool").astype("uint8")
+    minx, miny, maxx, maxy = mask2bbox_xyxy(fg_mask)
+
     bboxes = [anno["bbox"] for anno in annos]
     bbox_modes = [anno["bbox_mode"] for anno in annos]
     bboxes_xyxy = np.array(
-        [BoxMode.convert(box, box_mode, BoxMode.XYXY_ABS) for box, box_mode in zip(bboxes, bbox_modes)]
+        [BoxMode.convert(box, box_mode, BoxMode.XYXY_ABS)
+         for box, box_mode in zip(bboxes, bbox_modes)]
     )
     quats = [anno["quat"] for anno in annos]
     transes = [anno["trans"] for anno in annos]
@@ -102,53 +132,69 @@ for d in tqdm(dset_dicts):
 
     gt_Rs = []
     gt_ts = []
-
-    labels = []
+    gt_labels = []
 
     for anno_i, anno in enumerate(annos):
         obj_name = obj_names[anno_i]
+        gt_labels.append(objects.index(obj_name))  # 0-based label
 
+        gt_Rs.append(Rs[anno_i])
+        gt_ts.append(transes[anno_i])
+
+    if scene_im_id not in preds:
+        print(scene_im_id, "not detected")
+        continue
+    cur_preds = preds[scene_im_id]
+    kpts_2d_est = []
+    est_Rs = []
+    est_ts = []
+    est_labels = []
+    for pred_i, pred in enumerate(cur_preds):
         try:
-            R_est = preds[obj_name][file_name]["R"]
-            t_est = preds[obj_name][file_name]["t"]
-            score = preds[obj_name][file_name]["score"]
+            R_est = pred["R"]
+            t_est = pred["t"]
+            score = pred["score"]
+            obj_name = pred["obj_name"]
         except:
             continue
         if score < score_thr:
             continue
 
-        labels.append(objects.index(obj_name))  # 0-based label
-
         est_Rs.append(R_est)
         est_ts.append(t_est)
-        gt_Rs.append(Rs[anno_i])
-        gt_ts.append(transes[anno_i])
+        est_labels.append(objects.index(obj_name))  # 0-based label
 
     im_gray = mmcv.bgr2gray(img, keepdim=True)
     im_gray_3 = np.concatenate([im_gray, im_gray, im_gray], axis=2)
 
     gt_poses = [np.hstack([_R, _t.reshape(3, 1)]) for _R, _t in zip(gt_Rs, gt_ts)]
-    poses = [np.hstack([_R, _t.reshape(3, 1)]) for _R, _t in zip(est_Rs, est_ts)]
+    est_poses = [np.hstack([_R, _t.reshape(3, 1)]) for _R, _t in zip(est_Rs, est_ts)]
 
-    ren.render(labels, poses, K=K, image_tensor=image_tensor, background=im_gray_3)
+    ren.render(
+        est_labels,
+        est_poses,
+        K=K,
+        image_tensor=image_tensor,
+        background=im_gray_3,
+    )
     ren_bgr = (image_tensor[:, :, :3].detach().cpu().numpy() + 0.5).astype("uint8")
 
-    for label, gt_pose, est_pose in zip(labels, gt_poses, poses):
-        ren.render([label], [gt_pose], K=K, seg_tensor=seg_tensor)
+    for gt_label, gt_pose in zip(gt_labels, gt_poses):
+        ren.render([gt_label], [gt_pose], K=K, seg_tensor=seg_tensor)
         gt_mask = (seg_tensor[:, :, 0].detach().cpu().numpy() > 0).astype("uint8")
-
-        ren.render([label], [est_pose], K=K, seg_tensor=seg_tensor)
-        est_mask = (seg_tensor[:, :, 0].detach().cpu().numpy() > 0).astype("uint8")
-
         gt_edge = get_edge(gt_mask, bw=3, out_channel=1)
-        est_edge = get_edge(est_mask, bw=3, out_channel=1)
-
         ren_bgr[gt_edge != 0] = np.array(mmcv.color_val("blue"))
+
+    for est_label, est_pose in zip(est_labels, est_poses):
+        ren.render([est_label], [est_pose], K=K, seg_tensor=seg_tensor)
+        est_mask = (seg_tensor[:, :, 0].detach().cpu().numpy() > 0).astype("uint8")
+        est_edge = get_edge(est_mask, bw=3, out_channel=1)
         ren_bgr[est_edge != 0] = np.array(mmcv.color_val("green"))
 
     vis_im = ren_bgr
 
-    save_path_0 = osp.join(vis_dir, "{}_{:06d}_vis0.png".format(scene_id, im_id))
+    save_path_0 = osp.join(
+        vis_dir, "{}_{:06d}_vis0.png".format(scene_id, im_id))
     mmcv.imwrite(img, save_path_0)
 
     save_path = osp.join(vis_dir, "{}_{:06d}_vis1.png".format(scene_id, im_id))
