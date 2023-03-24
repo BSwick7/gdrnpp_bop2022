962a7a0bb646593fa743b65c8254621130486809
Brennan Swick <swick.70@osu.edu>
2023-03-24 20:36:28 +0000

    added single point of control for loading yolox and gdrn methods
    new lines 47-49 allow the user to specify a dataset with the correct camera name in the dataset and a specific scene number. The rest of the script handles changes. However, this does not allow the user to load a SO model, but those should be combined to an ensemble model and were not considered for real-time application.

--- core/gdrn_modeling/demo/demo_gdrn.py
+++ core/gdrn_modeling/demo/demo_gdrn.py
@@ -1,4 +1,8 @@
 # inference with detector, gdrn, and refiner
+import cv2
+import os
+from predictor_gdrn import GdrnPredictor
+from predictor_yolo import YoloPredictor
 import csv
 import os.path as osp
 import sys
@@ -7,14 +11,9 @@ PROJ_ROOT = osp.normpath(osp.join(cur_dir, "../../.."))
 sys.path.insert(0, PROJ_ROOT)
 
 
-from predictor_yolo import YoloPredictor
-from predictor_gdrn import GdrnPredictor
-import os
-
-import cv2
-
-
 IMAGE_EXT = [".jpg", ".jpeg", ".webp", ".bmp", ".png"]
+
+
 def get_image_list(rgb_images_path, depth_images_path=None):
     image_names = []
 
@@ -44,19 +43,45 @@ def get_image_list(rgb_images_path, depth_images_path=None):
 
 
 if __name__ == "__main__":
-    image_paths = get_image_list(osp.join(PROJ_ROOT,"datasets/debug_data/lmo/test/000002/rgb"), osp.join(PROJ_ROOT,"datasets/debug_data/lmo/test/000002/depth"))
+    # given a dataset name like lmo, ycbv, or tless, load the correct config from the configs/yolox/bop_pbr folder in this repo
+    dataset_name = "ycbv"
+    camera_type = "camera_uw"
+    scene_num = "000048"
+    rgb_img_path = f"datasets/debug_data/{dataset_name}/test/{scene_num}/rgb"
+    depth_img_path = f"datasets/debug_data/{dataset_name}/test/{scene_num}/depth"
+    image_paths = get_image_list(
+        osp.join(PROJ_ROOT, rgb_img_path), osp.join(PROJ_ROOT, depth_img_path))
+    # if the dataset_name is hb load the config ends with primesense_bop19 instead
+    if dataset_name == "hb":
+        yolox_shared_path = f"yolox/bop_pbr/yolox_x_640_augCozyAAEhsv_ranger_30_epochs_{dataset_name}_pbr_{dataset_name}_test_primsense_bop19"
+    else:
+        yolox_shared_path = f"yolox/bop_pbr/yolox_x_640_augCozyAAEhsv_ranger_30_epochs_{dataset_name}_pbr_{dataset_name}_bop_test"
+    yolox_config_file = f"configs/{yolox_shared_path}.py"
+    yolox_ckpt_file = f"output/{yolox_shared_path}/model_final.pth"
     yolo_predictor = YoloPredictor(
-                       exp_name="yolox-x",
-                       config_file_path=osp.join(PROJ_ROOT,"configs/yolox/bop_pbr/yolox_x_640_augCozyAAEhsv_ranger_30_epochs_lmo_pbr_lmo_bop_test.py"),
-                       ckpt_file_path=osp.join(PROJ_ROOT,"output/yolox/bop_pbr/yolox_x_640_augCozyAAEhsv_ranger_30_epochs_lmo_pbr_lmo_bop_test/model_final.pth"),
-                       fuse=True,
-                       fp16=False
-                     )
+        exp_name="yolox-x",
+        config_file_path=osp.join(PROJ_ROOT, yolox_config_file),
+        ckpt_file_path=osp.join(PROJ_ROOT, yolox_ckpt_file),
+        fuse=True,
+        fp16=False
+    )
+    # if the dataset_name is ycbv the gdrn dir is just the dataset name, otherwise the gdrn dir is the dataset name + _pbr
+    if dataset_name == "ycbv":
+        gdrn_dir = dataset_name
+    else:
+        gdrn_dir = f"{dataset_name}_pbr"
+
+    # given a dataset gdrn directory like lmo_pbr, ycbv_pbr, or tless_pbr, load the correct config from the configs/gdrn folder in this repo
+    gdrn_shared_path = f"gdrn/{gdrn_dir}/convnext_a6_AugCosyAAEGray_BG05_mlL1_DMask_amodalClipBox_classAware_{dataset_name}"
+    gdrn_config_file = f"configs/{gdrn_shared_path}.py"
+    gdrn_ckpt_file = f"output/{gdrn_shared_path}/model_final_wo_optim.pth"
+    camera_json = f"datasets/BOP_DATASETS/{dataset_name}/{camera_type}.json"
+    model_path = f"datasets/BOP_DATASETS/{dataset_name}/models"
     gdrn_predictor = GdrnPredictor(
-        config_file_path=osp.join(PROJ_ROOT,"configs/gdrn/lmo_pbr/convnext_a6_AugCosyAAEGray_BG05_mlL1_DMask_amodalClipBox_classAware_lmo.py"),
-        ckpt_file_path=osp.join(PROJ_ROOT,"output/gdrn/lmo_pbr/convnext_a6_AugCosyAAEGray_BG05_mlL1_DMask_amodalClipBox_classAware_lmo/model_final_wo_optim.pth"),
-        camera_json_path=osp.join(PROJ_ROOT,"datasets/BOP_DATASETS/lmo/camera.json"),
-        path_to_obj_models=osp.join(PROJ_ROOT,"datasets/BOP_DATASETS/lmo/models")
+        config_file_path=osp.join(PROJ_ROOT, gdrn_config_file),
+        ckpt_file_path=osp.join(PROJ_ROOT, gdrn_ckpt_file),
+        camera_json_path=osp.join(PROJ_ROOT, camera_json),
+        path_to_obj_models=osp.join(PROJ_ROOT, model_path)
     )
 
     for rgb_img, depth_img in image_paths:
@@ -64,14 +89,17 @@ if __name__ == "__main__":
         if depth_img is not None:
             depth_img = cv2.imread(depth_img, 0)
         outputs = yolo_predictor.inference(image=rgb_img)
-        data_dict = gdrn_predictor.preprocessing(outputs=outputs, image=rgb_img, depth_img=depth_img)
+        data_dict = gdrn_predictor.preprocessing(
+            outputs=outputs, image=rgb_img, depth_img=depth_img)
         # if the data directory does not exist, create it
-        if not os.path.exists(osp.join(PROJ_ROOT,"output/gdrn/lmo_pbr/convnext_a6_AugCosyAAEGray_BG05_mlL1_DMask_amodalClipBox_classAware_lmo/gdrn_demo")):
-            os.makedirs(osp.join(PROJ_ROOT,"output/gdrn/lmo_pbr/convnext_a6_AugCosyAAEGray_BG05_mlL1_DMask_amodalClipBox_classAware_lmo/gdrn_demo"))
+        demo_output_dir = f"output/{gdrn_shared_path}/gdrn_demo/{scene_num}"
+        if not os.path.exists(osp.join(PROJ_ROOT, demo_output_dir)):
+            os.makedirs(osp.join(
+                PROJ_ROOT, demo_output_dir))
         # write the data dictionary to a csv file in the output/gdrn/lmo_pbr/convnext_a6_AugCosyAAEGray_BG05_mlL1_DMask_amodalClipBox_classAware_lmo directory using csv.DictWriter
-        with open(osp.join(PROJ_ROOT,"output/gdrn/lmo_pbr/convnext_a6_AugCosyAAEGray_BG05_mlL1_DMask_amodalClipBox_classAware_lmo/gdrn_demo/data_dict.csv"), 'w') as f:
+        with open(osp.join(PROJ_ROOT, demo_output_dir + "/data_dict.csv"), 'w') as f:
             w = csv.writer(f)
-            # if a key has no value remove the key from the data dictionary 
+            # if a key has no value remove the key from the data dictionary
             for key in list(data_dict.keys()):
                 if len(data_dict[key]) == 0:
                     del data_dict[key]
@@ -83,9 +111,9 @@ if __name__ == "__main__":
                 w.writerow([data_dict[x].tolist()[i] for x in key_list])
         out_dict = gdrn_predictor.inference(data_dict)
         # write the dictionary to a csv file in the output/gdrn/lmo_pbr/convnext_a6_AugCosyAAEGray_BG05_mlL1_DMask_amodalClipBox_classAware_lmo directory using csv.DictWriter
-        with open(osp.join(PROJ_ROOT,"output/gdrn/lmo_pbr/convnext_a6_AugCosyAAEGray_BG05_mlL1_DMask_amodalClipBox_classAware_lmo/gdrn_demo/output_dict.csv"), 'w') as f:
+        with open(osp.join(PROJ_ROOT, demo_output_dir + "/output_dict.csv"), 'w') as f:
             w = csv.writer(f)
-            # if a key has no value remove the key from the data dictionary 
+            # if a key has no value remove the key from the data dictionary
             for key in list(out_dict.keys()):
                 if len(out_dict[key]) == 0:
                     del out_dict[key]
@@ -97,9 +125,9 @@ if __name__ == "__main__":
                 w.writerow([out_dict[x].tolist()[i] for x in key_list])
         poses = gdrn_predictor.postprocessing(data_dict, out_dict)
         # write the poses to a csv file in the output/gdrn/lmo_pbr/convnext_a6_AugCosyAAEGray_BG05_mlL1_DMask_amodalClipBox_classAware_lmo directory using csv.DictWriter
-        with open(osp.join(PROJ_ROOT,"output/gdrn/lmo_pbr/convnext_a6_AugCosyAAEGray_BG05_mlL1_DMask_amodalClipBox_classAware_lmo/gdrn_demo/poses.csv"), 'w') as f:
+        with open(osp.join(PROJ_ROOT, demo_output_dir + "/poses.csv"), 'w') as f:
             w = csv.writer(f)
-            # if a key has no value remove the key from the data dictionary 
+            # if a key has no value remove the key from the data dictionary
             for key in list(poses.keys()):
                 if len(poses[key]) == 0:
                     del poses[key]
@@ -108,6 +136,6 @@ if __name__ == "__main__":
             w.writerow(poses.keys())
             for i in range(limit):
                 # if the key has an no value, skip it
-                w.writerow([poses[x].tolist()[i] for x in key_list]) 
-        gdrn_predictor.gdrn_visualization(batch=data_dict, out_dict=out_dict, image=rgb_img)
-
+                w.writerow([poses[x].tolist()[i] for x in key_list])
+        gdrn_predictor.gdrn_visualization(
+            batch=data_dict, out_dict=out_dict, image=rgb_img)
